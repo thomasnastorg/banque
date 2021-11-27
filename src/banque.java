3bed1559d6da904e691df0f1c603410a0c9759e1
Thomas <nastorgt62@gmail.com>
2021-11-27 16:59:35 +0100

    tt

--- src/banque.java
+++ src/banque.java
@@ -1,28 +1,73 @@
 import javax.swing.*;
 
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class banque {
+
     private JButton button1;
     private JTextField textField1;
     private JTextArea textArea1;
-    private JPanel jpmain;
-    private JMenu operation;
-    private JMenu modification;
-    private JMenuItem Créditer;
+    private  JPanel jpmain;
     private JLabel etat;
+    private JMenuBar myMenuBar;
+    private JMenu MenuOperation;
+    private JMenu MenuModification;
+    private JMenuItem myDebiter;
+    public JMenuItem myCredit;
+    private JMenuItem myDecouvert;
+    private JMenuItem myClient;
+    private Boolean toto = false;
 
     public banque() {
         button1.setVisible(false);
-        textArea1.setVisible(false);
         textField1.setVisible(false);
         etat.setVisible(false);
+
+
+        myCredit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                etat.setVisible(true);
+                etat.setText("Montant a craditer");
+                textField1.setVisible(true);
+                button1.setVisible(true);
+                button1.setText("crediter");
+            }
+
+        });
+        myDebiter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                etat.setVisible(true);
+                etat.setText("Montant a debiter");
+                textField1.setVisible(true);
+                button1.setVisible(true);
+                button1.setText("Debiter");
+            }
+        });
+
+        myClient.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(toto == false ) {
+                    JFrame frame1 = new JFrame("User");
+                    frame1.setContentPane(new user().jpuser);
+                    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame1.setSize(10000, 10000);
+                    frame1.pack();
+                    frame1.setVisible(true);
+                    frame1.setLocationRelativeTo(null);
+                    toto = true;
+                }
+            }
+        });
 
 
 
     }
     public static void main(String[] args) {
-        JFrame frame = new JFrame("uipendu");
+        JFrame frame = new JFrame("Banque");
         frame.setContentPane(new banque().jpmain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(10000,10000);
@@ -30,6 +75,14 @@ public class banque {
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
 
+        /*
+        JMenuBar myMenuBar = new JMenuBar();
+        frame.add(myMenuBar);
+        myMenuBar.setBounds(0,0,1000,1000);
+        JMenu MenuOperation = new JMenu("Operation");
+        myMenuBar.add(MenuOperation);
+        frame.setVisible(true);
+        */
 
 
     }
---
+++ src/user.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class user {
+
+
+
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton validerButton;
+    public JPanel jpuser;
+    private JLabel myNum;
+    private JLabel muNom;
+    private JLabel myPrenom;
+    private JLabel myAdresse;
+
+
+
+    user(){
+
+
+
+
+    }
+}
